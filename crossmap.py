3bd1656ede36514ed64c8f65e48ad4b9607e100a
Hrant Hovhannisyan <grant.hovhannisyan@gmail.com>
2018-11-09 15:04:34 +0100

    Added annotation testing

--- crossmap.py
+++ crossmap.py
@@ -11,19 +11,28 @@ import argparse
 import subprocess 
 
 
+
+desc = """
+  --
+  crossmap.py Software
+"""
+
+soft_version = "0.1"
+
 ###Create top level parser
 
-#formatter_class=argparse.RawTextHelpFormatter
+#formatter_class=argparse.RawTextHelpFormatter - with this the text in help is free of formating
+#formatter_class=argparse.ArgumentDefaultsHelpFormatter - this adds default values in help. Should be in all subparsers.
 
-parser = argparse.ArgumentParser(prog = "crossmap",)
-subparsers = parser.add_subparsers(help="sub_command help")
+parser = argparse.ArgumentParser(prog = "crossmap.py",description = desc, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparsers = parser.add_subparsers(help="sub_command help", dest = "Simulation_type")
 
-parser_DNA = subparsers.add_parser("DNA", help = "Simulate DNA data")
+parser_DNA = subparsers.add_parser("DNA", help = "Simulate DNA data",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_DNA.add_argument("-specific to bwa", "--specific", type=int, default=5,
 	help = "Help message")
 	
-parser_RNA = subparsers.add_parser("RNA", help = "Simulate RNA data")
-parser_RNA.add_argument("-max_mismatch", "--outFilterMismatchNmax", type=int, default=10,
+parser_RNA = subparsers.add_parser("RNA", help = "Simulate RNA data", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_RNA.add_argument("-max_mismatch", "--outFilterMismatchNmax", type=int, default=10, metavar="Int",
 	help = "From STAR manual: "
 	+ " alignment will be output only if it has no more mismatches than this value")
 
@@ -92,47 +101,81 @@ parser.add_argument("-A", "--discard_ambig", type=float, default = 0.05,
 	
 parser.add_argument("-hapl", "--haplotype_mode", action = "store_true", default = False,
 	help = "Haplotype mode. If specified, the haploid mutations will be simulated instead of diploid.")
-
-args = parser.parse_args()
-
-
-#~ ### BWA PARAMETERS
-#~ ## For indexing
-
-#~ -p STR 	Prefix of the output database [same as db filename]
-#~ -a STR 	Algorithm for constructing BWT index. Available options are:
-#~ is 	IS linear-time algorithm for constructing suffix array. It requires 5.37N memory where N is the size of the database. IS is moderately fast, but does not work with database larger than 2GB. IS is the default algorithm due to its simplicity. The current codes for IS algorithm are reimplemented by Yuta Mori.
-#~ bwtsw 	Algorithm implemented in BWT-SW. This method works with the whole human genome.
-
-#~ ## For mapping
-#~ ### -t INT 	Number of threads [1] 
-#~ -k INT 	Minimum seed length. Matches shorter than INT will be missed. The alignment speed is usually insensitive to this value unless it significantly deviates 20. [19]
-#~ -w INT 	Band width. Essentially, gaps longer than INT will not be found. Note that the maximum gap length is also affected by the scoring matrix and the hit length, not solely determined by this option. [100]
-#~ -d INT 	Off-diagonal X-dropoff (Z-dropoff). Stop extension when the difference between the best and the current extension score is above |i-j|*A+INT, where i and j are the current positions of the query and reference, respectively, and A is the matching score. Z-dropoff is similar to BLAST’s X-dropoff except that it doesn’t penalize gaps in one of the sequences in the alignment. Z-dropoff not only avoids unnecessary extension, but also reduces poor alignments inside a long good alignment. [100]
-#~ -r FLOAT 	Trigger re-seeding for a MEM longer than minSeedLen*FLOAT. This is a key heuristic parameter for tuning the performance. Larger value yields fewer seeds, which leads to faster alignment speed but lower accuracy. [1.5]
-#~ -c INT 	Discard a MEM if it has more than INT occurence in the genome. This is an insensitive parameter. [10000]
-#~ -P 	In the paired-end mode, perform SW to rescue missing hits only but do not try to find hits that fit a proper pair.
-#~ -A INT 	Matching score. [1]
-#~ -B INT 	Mismatch penalty. The sequence error rate is approximately: {.75 * exp[-log(4) * B/A]}. [4]
-#~ -O INT 	Gap open penalty. [6]
-#~ -E INT 	Gap extension penalty. A gap of length k costs O + k*E (i.e. -O is for opening a zero-length gap). [1]
-#~ -L INT 	Clipping penalty. When performing SW extension, BWA-MEM keeps track of the best score reaching the end of query. If this score is larger than the best SW score minus the clipping penalty, clipping will not be applied. Note that in this case, the SAM AS tag reports the best SW score; clipping penalty is not deducted. [5]
-#~ -U INT 	Penalty for an unpaired read pair. BWA-MEM scores an unpaired read pair as scoreRead1+scoreRead2-INT and scores a paired as scoreRead1+scoreRead2-insertPenalty. It compares these two scores to determine whether we should force pairing. [9]
-#~ ###-p 	Assume the first input query file is interleaved paired-end FASTA/Q. See the command description for details.
-#~ -R STR 	Complete read group header line. ’\t’ can be used in STR and will be converted to a TAB in the output SAM. The read group ID will be attached to every read in the output. An example is ’@RG\tID:foo\tSM:bar’. [null]
-#~ -T INT 	Don’t output alignment with score lower than INT. This option only affects output. [30]
-#~ ### this should be default -a 	Output all found alignments for single-end or unpaired paired-end reads. These alignments will be flagged as secondary alignments.
-#~ #-C 	Append append FASTA/Q comment to SAM output. This option can be used to transfer read meta information (e.g. barcode) to the SAM output. Note that the FASTA/Q comment (the string after a space in the header line) must conform the SAM spec (e.g. BC:Z:CGTAC). Malformated comments lead to incorrect SAM output.
-#~ -H 	Use hard clipping ’H’ in the SAM output. This option may dramatically reduce the redundancy of output when mapping long contig or BAC sequences.
-#~ -M 	Mark shorter split hits as secondary (for Picard compatibility).
-#~ -v INT 	Control the verbose level of the output. This option has not been fully supported throughout BWA. Ideally, a value 0 for disabling all the output to stderr; 1 for outputting errors only; 2 for warnings and errors; 3 for all normal messages; 4 or higher for debugging. When this option takes value 4, the output is not SAM. [3] 
+	
+parser.add_argument("-v", "--version", action = "version", \
+    version = "%(prog)s \"v" + soft_version + "\"")
 
 
-#~ ### STAR parameters, For now I will use only basic parameters:
+parsedArgs =  parser.parse_args()
+#~ print(len(parsedArgs.annotations))
 
-#~ ## For indexing
 
-#~ ## For mapping
+## If no arguments are given, just show the help and finish
+if len(sys.argv) == 1:
+   parser.print_help()
+   sys.exit(1)
+
+
+if parsedArgs.Simulation_type == "RNA":
+    print("Simulating RNA")
+	 
+else:
+	print("Simulating DNA")
+	
+
+def getBaseName(filename):
+	basename = '.'.join(filename.split(".")[0:-1])
+	return basename
+	
+def extractTranscriptome():
+    transcriptome_names=[]
+    for i in range(0,len(parsedArgs.annotations)):
+        if parsedArgs.annotations[i].split(".")[-1] == "gtf":
+            print("Annotation file %s detected as gtf. Continuing."%(i+1))
+            #get the transcriptome name
+            transcriptome_name=getBaseName(parsedArgs.genomes[i])+"_transcriptome_%s"%(i+1)+".fasta"
+            
+            # extract the transcript
+            #gffread -w transcriptome_name -g parsedArgs.genomes[i] parsedArgs.annotations[i]
+            
+            # add the name to list
+            transcriptome_names.append(transcriptome_name)
+            print(transcriptome_names)
+            
+        elif parsedArgs.annotations[i].split(".")[-1] == "gff":
+			
+            print("Annotation file %s detected as gff. Converting to gtf using gffread"%(i+1))
+            
+            #converting to gtf
+            gtf_name = getBaseName(parsedArgs.genomes[i])+".gtf"
+            
+            #gffread parsedArgs.annotations[i] -T -o gtf_name
+            
+            print("GFF --> GTF conversion is done")
+            
+            #get the transcriptome name
+            transcriptome_name = getBaseName(parsedArgs.genomes[i])+"_transcriptome_%s"%(i+1)+".fasta"
+            
+            # extract the transcript
+            #gffread -w transcriptome_name -g parsedArgs.genomes[i] gtf_name
+            
+            
+            # add the name to list
+            transcriptome_names.append(transcriptome_name)
+            print(transcriptome_names)
+
+        else:
+            sys.exit("Error: annotation file %s is neither in gtf nor in gff format. Please check the annotation file."%(i))
+            
+    return transcriptome_names
+    print(transcriptome_names)
+    
+#print transcriptome_names
+	
+
+if parsedArgs.Simulation_type == "RNA":
+   extractTranscriptome()
+
 
 
 #~ ### Gffread command
@@ -234,6 +277,46 @@ args = parser.parse_args()
 #~ define --sjdbOverhang = rlen-1
 
 
+
+######################################
+
+#~ ### BWA PARAMETERS
+#~ ## For indexing
+
+#~ -p STR 	Prefix of the output database [same as db filename]
+#~ -a STR 	Algorithm for constructing BWT index. Available options are:
+#~ is 	IS linear-time algorithm for constructing suffix array. It requires 5.37N memory where N is the size of the database. IS is moderately fast, but does not work with database larger than 2GB. IS is the default algorithm due to its simplicity. The current codes for IS algorithm are reimplemented by Yuta Mori.
+#~ bwtsw 	Algorithm implemented in BWT-SW. This method works with the whole human genome.
+
+#~ ## For mapping
+#~ ### -t INT 	Number of threads [1] 
+#~ -k INT 	Minimum seed length. Matches shorter than INT will be missed. The alignment speed is usually insensitive to this value unless it significantly deviates 20. [19]
+#~ -w INT 	Band width. Essentially, gaps longer than INT will not be found. Note that the maximum gap length is also affected by the scoring matrix and the hit length, not solely determined by this option. [100]
+#~ -d INT 	Off-diagonal X-dropoff (Z-dropoff). Stop extension when the difference between the best and the current extension score is above |i-j|*A+INT, where i and j are the current positions of the query and reference, respectively, and A is the matching score. Z-dropoff is similar to BLAST’s X-dropoff except that it doesn’t penalize gaps in one of the sequences in the alignment. Z-dropoff not only avoids unnecessary extension, but also reduces poor alignments inside a long good alignment. [100]
+#~ -r FLOAT 	Trigger re-seeding for a MEM longer than minSeedLen*FLOAT. This is a key heuristic parameter for tuning the performance. Larger value yields fewer seeds, which leads to faster alignment speed but lower accuracy. [1.5]
+#~ -c INT 	Discard a MEM if it has more than INT occurence in the genome. This is an insensitive parameter. [10000]
+#~ -P 	In the paired-end mode, perform SW to rescue missing hits only but do not try to find hits that fit a proper pair.
+#~ -A INT 	Matching score. [1]
+#~ -B INT 	Mismatch penalty. The sequence error rate is approximately: {.75 * exp[-log(4) * B/A]}. [4]
+#~ -O INT 	Gap open penalty. [6]
+#~ -E INT 	Gap extension penalty. A gap of length k costs O + k*E (i.e. -O is for opening a zero-length gap). [1]
+#~ -L INT 	Clipping penalty. When performing SW extension, BWA-MEM keeps track of the best score reaching the end of query. If this score is larger than the best SW score minus the clipping penalty, clipping will not be applied. Note that in this case, the SAM AS tag reports the best SW score; clipping penalty is not deducted. [5]
+#~ -U INT 	Penalty for an unpaired read pair. BWA-MEM scores an unpaired read pair as scoreRead1+scoreRead2-INT and scores a paired as scoreRead1+scoreRead2-insertPenalty. It compares these two scores to determine whether we should force pairing. [9]
+#~ ###-p 	Assume the first input query file is interleaved paired-end FASTA/Q. See the command description for details.
+#~ -R STR 	Complete read group header line. ’\t’ can be used in STR and will be converted to a TAB in the output SAM. The read group ID will be attached to every read in the output. An example is ’@RG\tID:foo\tSM:bar’. [null]
+#~ -T INT 	Don’t output alignment with score lower than INT. This option only affects output. [30]
+#~ ### this should be default -a 	Output all found alignments for single-end or unpaired paired-end reads. These alignments will be flagged as secondary alignments.
+#~ #-C 	Append append FASTA/Q comment to SAM output. This option can be used to transfer read meta information (e.g. barcode) to the SAM output. Note that the FASTA/Q comment (the string after a space in the header line) must conform the SAM spec (e.g. BC:Z:CGTAC). Malformated comments lead to incorrect SAM output.
+#~ -H 	Use hard clipping ’H’ in the SAM output. This option may dramatically reduce the redundancy of output when mapping long contig or BAC sequences.
+#~ -M 	Mark shorter split hits as secondary (for Picard compatibility).
+#~ -v INT 	Control the verbose level of the output. This option has not been fully supported throughout BWA. Ideally, a value 0 for disabling all the output to stderr; 1 for outputting errors only; 2 for warnings and errors; 3 for all normal messages; 4 or higher for debugging. When this option takes value 4, the output is not SAM. [3] 
+
+
+#~ ### STAR parameters, For now I will use only basic parameters:
+
+#~ ## For indexing
+
+#~ ## For mapping
 
 
 
